ece938431acb5afbea3b0c5db72f755ef900581c
Matteo Monti <monti@MacBook-Pro-di-Matteo.local>
2016-03-31 20:40:25 +0200

    Refactored request to needle.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function(potty)
   var electron = require('electron');
 
   var nappy = require('nappy');
-  var request = require('request');
+  var needle = require('request');
   var path = require('path');
   var os = require('os');
 
@@ -17,9 +17,9 @@ module.exports = function(potty)
 
     nappy.wait.for(settings.update.interval).then(nappy.wait.connection).then(function()
     {
-      request('https://rain.vg/releases/' + pkg.name + '/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/production/package', function(error, response, body)
+      needle.get('https://rain.vg/releases/' + pkg.name + '/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/production/package', function(error, response)
       {
-        if(!error && response.statusCode === 200 && JSON.parse(body).version !== pkg.version)
+        if(!error && response.statusCode === 200 && JSON.parse(response.body).version !== pkg.version)
         {
           potty.update().then(function()
           {
