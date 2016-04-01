8d195b7410ee2534a61bbefa99a141334049f2dc
Marco Moschettini <m.marmos@gmail.com>
2016-04-01 20:35:26 +0200

    Possible bugfix for apparent garbage collector related problem.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
-module.exports = function(potty)
+var electron = require('electron');
+
+var nappy = require('nappy');
+var needle = require('needle');
+var path = require('path');
+var os = require('os');
+
+var pkg = require(path.resolve(__dirname, '..', 'package.json'));
+
+// Settings
+
+var settings = {update: {interval: 300000}};
+
+// Members
+
+var _potty;
+var _appIcon;
+
+// Private methods
+
+var __update__ = function()
 {
-  var electron = require('electron');
+  'use strict';
 
-  var nappy = require('nappy');
-  var needle = require('needle');
-  var path = require('path');
-  var os = require('os');
-
-  var pkg = require(path.resolve(__dirname, '..', 'package.json'));
-
-  var settings = {update: {interval: 300000}};
-
-  (function __update__()
+  nappy.wait.for(settings.update.interval).then(nappy.wait.connection).then(function()
   {
-    'use strict';
-
-    nappy.wait.for(settings.update.interval).then(nappy.wait.connection).then(function()
+    needle.get('https://rain.vg/releases/' + pkg.name + '/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/production/package', function(error, response)
     {
-      needle.get('https://rain.vg/releases/' + pkg.name + '/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/production/package', function(error, response)
+      if(!error && response.statusCode === 200 && JSON.parse(response.body).version !== pkg.version)
       {
-        if(!error && response.statusCode === 200 && JSON.parse(response.body).version !== pkg.version)
+        _potty.update().then(function()
         {
-          potty.update().then(function()
-          {
-            electron.app.exit(0);
-          });
-        }
+          electron.app.exit(0);
+        });
+      }
 
-        __update__();
-      });
+      __update__();
     });
-  })();
+  });
+};
 
-  var appIcon = new electron.Tray(path.resolve(__dirname, '..', 'resources', 'logo.png'));
+var __setup__ = function()
+{
+  _appIcon = new electron.Tray(path.resolve(__dirname, '..', 'resources', 'logo.png'));
 
   var contextMenu = electron.Menu.buildFromTemplate([
     {label: 'Rain version ' + pkg.version},
@@ -40,7 +49,14 @@ module.exports = function(potty)
     {label: '❤ for contributing!'}
   ]);
 
-  appIcon.setToolTip('Rain');
-  appIcon.setContextMenu(contextMenu);
+  _appIcon.setToolTip('Rain');
+  _appIcon.setContextMenu(contextMenu);
+};
 
+module.exports = function(potty)
+{
+  _potty = potty;
+
+  __setup__();
+  __update__();
 };
