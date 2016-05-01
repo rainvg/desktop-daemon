2a89705168cbb16b4325aee3768e3ef45babd1d4
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-05-02 01:21:48 +0200

    Fixed bug on update. All energy relevant events added.

--- src/update.js
+++ src/update.js
@@ -10,17 +10,26 @@ var pkg = require(path.resolve(__dirname, '..', 'package.json'));
 
 var settings = {update: {interval: 300000}};
 
-module.exports = function __update__(potty)
+// Members
+
+var _potty;
+
+var __update__ = function()
 {
   'use strict';
 
   nappy.wait.for(settings.update.interval).then(nappy.wait.connection).then(function()
   {
+    console.log('Looking for updates..');
+    console.log('https://rain.vg/releases/' + pkg.name + '/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/' + pkg.scheme + '/package');
     needle.get('https://rain.vg/releases/' + pkg.name + '/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/' + pkg.scheme + '/package', function(error, response)
     {
-      if(!error && response.statusCode === 200 && JSON.parse(response.body).version !== pkg.version)
+      console.log('Response is', response.body);
+
+      if(!error && response.statusCode === 200 && JSON.parse(response.body.toString()).version !== pkg.version)
       {
-        potty.update().then(function()
+        console.log('Updating.');
+        _potty.update().then(function()
         {
           electron.app.exit(0);
         });
@@ -29,4 +38,10 @@ module.exports = function __update__(potty)
       __update__();
     });
   });
+};
+
+module.exports = function(potty)
+{
+  _potty = potty;
+  __update__();
 };
