822ee183c467296f3089db9bd1eba3e731e63f46
Marco Moschettini <m.marmos@gmail.com>
2016-05-02 17:35:07 +0200

    Now desktop-daemon should clear old setups on windows.

--- src/autostart.js
+++ src/autostart.js
@@ -2,6 +2,7 @@ var autolaunch = require('auto-launch');
 var fs = require('fs-extra');
 var os = require('os');
 var path = require('path');
+var registry = require('winreg');
 
 function __run__()
 {
@@ -14,18 +15,43 @@ function __run__()
 
   login_item.isEnabled().then(function(enabled)
   {
-    console.log(enabled);
     if(enabled) return;
     login_item.enable();
   });
+}
 
+function __clean__()
+{
+  'use strict';
+  
   if(os.platform() === 'darwin')
   {
-    // LaunchAgent fix
-
     var plist = path.resolve(os.homedir(), 'Library', 'LaunchAgents', 'vg.rain.osx.plist');
     if(fs.existsSync(plist))
       fs.remove(plist);
+  }
+  else if(os.platform() === 'win32')
+  {
+    var registry_entry = new registry({
+      hive: registry.HKCU,
+      key: '\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders'
+    });
+
+    registry_entry.values(function(error, items)
+    {
+      if(!error)
+      {
+        for (var i=0; i < items.length; i++)
+        {
+          if(items[i].name === 'Startup')
+          {
+            var rain_link = path.join(items[i].value, 'Rain.lnk');
+            if(fs.existsSync(rain_link))
+              fs.remove(rain_link);
+          }
+        }
+      }
+    });
   }
 }
 
@@ -34,4 +60,5 @@ module.exports = function autostart()
   'use strict';
 
   __run__();
+  __clean__();
 };
