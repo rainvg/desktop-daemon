2baadf0ccea5bbcc92699b4c0fa45ca19a5cdc5b
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-05-19 10:46:22 +0200

    Added random phase switching to avoid systematic sampling error.

--- src/daemons/square-wave-downloader.js
+++ src/daemons/square-wave-downloader.js
@@ -11,6 +11,8 @@ var settings = {period: 3600000, interval: 60000, size: 1048576, endpoint:'https
 var _events;
 var _analytics;
 
+var _level = {time_slice: 0, phase: false};
+
 var download = function()
 {
   return (os.platform() === 'darwin' || os.platform() === 'win32') ? new Promise(function(resolve, reject)
@@ -43,11 +45,23 @@ var download = function()
   }) : Promise.reject();
 };
 
+var __level__ = function()
+{
+  var current_time_slice = Math.floor(new Date().getTime() / settings.period);
+
+  if(current_time_slice !== _level.time_slice)
+  {
+    _level.time_slice = current_time_slice;
+    _level.phase = (Math.random() < 0.5);
+  }
+
+  var half = (new Date().getTime() % settings.period) > (settings.period / 2);
+  return (_level.phase && !half) || (!(_level.phase) && half);
+};
+
 var __run__ = function()
 {
-  var level = (new Date().getTime() % settings.period) > (settings.period / 2);
-
-  if(level)
+  if(__level__())
     download().then(function()
     {
       _events.push({type: 'square-wave-downloader'});
