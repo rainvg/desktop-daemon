6b02c47d3b8837a0293dbc2db38562c1d32ada73
Matteo Monti <matteo.monti@msoftprogramming.com>
2016-05-27 12:23:38 +0200

    fs-extra dependency used everywhere.

--- src/daemons.js
+++ src/daemons.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var fs = require('fs');
+var fs = require('fs-extra');
 var needle = require('needle');
 var nappy = require('nappy');
 var universal_analytics = require('universal-analytics');
